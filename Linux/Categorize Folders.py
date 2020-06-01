adb8c784e0430cf1a33a4b2a0825fd8e3dd0fa2c
BijuAle <bijuale@hotmail.com>
2020-06-01 12:28:22 +0545

    pdfgrep

--- Linux/Categorize Folders.py
+++ Linux/Categorize Folders.py
@@ -1,17 +1,19 @@
 import os, shutil, sys
 
-def organize(searchString):    
+def organize(searchString):
     
-    searchPath='/home/biju/Study/Books/Mathematics/Math Library/Miscellany/'
-    destPath='/home/biju/Study/Books/Mathematics/Math Library/'
+    #searchPath='/home/biju/Study/Books/Mathematics/Math Library/Miscellany/'
+    searchPath = os.getcwd()
+    destPath = os.getcwd()
+    #destPath='/home/biju/Study/Books/Mathematics/Math Library/'
     
     for folderName,subfolders, filenames in os.walk(searchPath):
         for filename in filenames:
             if filename.__contains__(searchString):
-                print ('Moving: '+filename)
-                if not (os.path.exists(destPath+searchString)):
-                    os.makedirs(destPath+searchString)
-                shutil.move(searchPath+filename, destPath+searchString)
+                print ('Moving: ' + filename)
+                if not (os.path.exists(destPath + '/' + searchString)):
+                    os.makedirs(destPath + '/' + searchString)
+                shutil.move(searchPath + '/' + filename, destPath + '/' + searchString)
 
 if __name__ == '__main__':
     organize(sys.argv[1])
