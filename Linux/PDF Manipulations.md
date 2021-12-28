7ed60f0807f91b4f783a21b7825fb52d1d19079a
BijuAle <bijuale@hotmail.com>
2021-12-28 19:18:49 +0545

    Updated

--- Linux/PDF Manipulations.md
+++ Linux/PDF Manipulations.md
@@ -2,15 +2,16 @@
 
 ## Convert JPG image to PDF
 ``` bash
-convert <filename>.jpg -auto-orient <filename>.pdf
-```
+for i in *jpeg; do
+	convert "$i" -auto-orient name>.pdf
+done
 
+```
 ## Combine Images to PDF
 
 ```bash
 convert *.jpg -auto-orient pictures.pdf
 ```
-
 ## Resize PDF to A4-paper
 
 ```bash
@@ -18,16 +19,27 @@ for pdf in *; do
 	pdfjam --outfile $pdf --paper a4paper $pdf
 done
 ```
-
 ## Search text within PDF
 ```bash
-pdfgrep -r --include *.pdf <search-text>
+pdfgrep -C 3 -HiR -e Theseaus *.pdf
 ```
-
 ## Trim first page
 ```bash
 mkdir trimmed
 for i in *pdf;
 	do pdftk "$i" cat 2-end output "trimmed/$i";
+done
+```
+## Extract Images from PDF
+
+```bash
+mkdir extracted-images
+pdfimages -all <path-to-pdf> <path-to-'extracted-images'>/image
+```
+
+## Multiple djvu to pdf
+```bash 
+for i in *.djvu; 
+	do djvu2pdf "$i" "${i/%.djvu/}.pdf";
 done
 ```
--- Linux/ScrapeLibGen.py
+++ Linux/ScrapeLibGen.py
@@ -1,5 +1,4 @@
-import os
-import sys
+import os, sys, re, time
 from urllib.request import urlopen
 from selenium import webdriver
 from selenium.webdriver.common.by import By
@@ -9,41 +8,84 @@ from selenium.webdriver.chrome.options import Options
 class Scraper:
     def __init__(self, webtable):
         self.table = webtable
+        self.links = []
+        self.downloadLinks = []
+        self.authors = []
+        self.titles = []
 
-    def getDownloadLinks(self):
-        sys.stdout.write('\r'+'Fetching Download Links.\n')
-        downloadLinks = []
-        for eachLink in self.getLinksList():
-            driver.get(eachLink)
-            dl = driver.find_element_by_xpath("//*[@id='download']/h2/a")
-            downloadLinks.append(dl.get_attribute('href'))
-        self.writeDownloadLinksToFile(downloadLinks)
-    
-    def getLinksList(self, column_number=10):
-        sys.stdout.write(
-            '\r'+'Scanning Complete.\nPreparing Links For Download...\n')
-        linksColumn = self.table.find_elements_by_xpath(
-            "//tr/td["+str(column_number)+"]/a")
-        LinksList = []
+    def getLinks(self):
+        sys.stdout.write("\r" + "Fetching Download Links.\n")
+        for link in self.getDownloadLinks():
+            driver.get(link)
+            dl = driver.find_element(By.XPATH, '//*[@id="download"]/ul/li[1]/a')
+            self.downloadLinks.append(dl.get_attribute("href"))
+
+        self.getList(list_type="titles", col_no=3)
+        # self.getList(list_type="authors", col_no=2)
+        # self.writeToFile(self.downloadLinks)
+
+    def getDownloadLinks(self, column_number=10):
+        sys.stdout.write("\r" + "Preparing Links For Download...\n")
+        linksColumn = self.table.find_elements(
+            By.XPATH, "//tr/td[" + str(column_number) + "]/a"
+        )
         for webElement in linksColumn:
-            LinksList.append(webElement.get_attribute('href'))
+            self.links.append(webElement.get_attribute("href"))
+        sys.stdout.write("\r" + "Links Prepared.\n")
+        return self.links
 
-        sys.stdout.write('\r'+'Links Prepared.\n')
-        return LinksList
+    def writeToFile(self, downloadLinks):
+        sys.stdout.write("\r" + "Writing Download Links To File...\n")
+        with open(os.getcwd() + "/download_links.txt", "w") as f:
+            for line in downloadLinks:
+                f.write("%s\n" % line)
+                os.system("wget -c " + line)
+        sys.stdout.write("\r" + "File Ready. \n")
 
-    def writeDownloadLinksToFile(self, downloadLinks):
-        sys.stdout.write('\r'+'Writing Download Links To File...\n')
-        with open(os.getcwd()+'/file.txt', 'w') as f:
-            for item in downloadLinks:
-                f.write("%s\n" % item)
-        sys.stdout.write('\r'+'File Ready.\n')
+    def getList(self, list_type, col_no):
+        sys.stdout.write("\r" + "Preparing List of " + list_type + " for Download...\n")
+        column = self.table.find_elements(By.XPATH, "//tr/td[" + str(col_no) + "]")
+        sys.stdout.write("\r" + "Fetching " + list_type + "\n")
+        if list_type == "titles":
+            for webElement in column:
+                txt = (webElement.text).replace("\n", " ")
+                txt = txt.replace(", ", "")
+                # replace trailing digits from end of string:
+                txt = re.sub("^\d+\s|\s\d+\s|\s\d+$", " ", txt)
+                self.titles.append(txt)
+            sys.stdout.write(list_type + " list prepared.\n")
+            del self.titles[:2]
+            del self.titles[-1]
+            sys.stdout.write(
+                "\n".join(str(str(i + 1) + "\t" + x) for i, x in enumerate(self.titles))
+            )
+            return self.titles
+        elif list_type == "authors":
+            for webElement in column:
+                txt = (webElement.text).replace("\n", " ")
+                txt = txt.replace(", ", " ")
+                txt = re.sub("^\d+\s|\s\d+\s|\s\d+$", " ", txt)
+                self.authors.append(txt)
+            sys.stdout.write(list_type + " list prepared.\n")
+            del self.authors[:3]
+            del self.authors[-1]
+            sys.stdout.write(
+                "\n".join(
+                    str(str(i + 1) + "\t" + x) for i, x in enumerate(self.authors)
+                )
+            )
+            return self.authors
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     options = Options()
+    options.binary_location = "/bin/brave"
     options.add_argument("--headless")
-    driver = webdriver.Chrome(options=options)
 
-    sys.stdout.write('\r'+'Scanning for Links...\n')
+    driver = webdriver.Chrome(
+        options=options, executable_path="/home/biju/.local/share/chromedriver"
+    )
+    sys.stdout.write("\r" + "Scanning for Links...\n")
     driver.get(sys.argv[1])
-    Scraper(driver.find_element_by_xpath(
-        "//table[@class='c']")).getDownloadLinks()
+
+    Scraper(driver.find_element(By.XPATH, "//table[@class='c']")).getLinks()
